9394ec702a328434c77d55dd533a53504770762e
Jayden-Liang <liangjisong@foxmail.com>
2020-03-03 21:40:01 +0800

    3.3

--- project/src/Components/Reading/Reading.vue
+++ project/src/Components/Reading/Reading.vue
@@ -18,14 +18,38 @@
                    </div>
                </div>
                </div>
-               <div class='readingChart'>
-                   <canvas id='myChart' ></canvas>
+               <div class='chart-pic'>
+                     <div class='readingChart' style='width: 35rem; height: 22rem;' >
+                      <canvas id='myChart' ></canvas>
+                    </div>
+                   <div class='pie'>
+                       <canvas id='pie-chart'></canvas>
+                  </div>
                </div>
+
+               <div class='daily-say'>
+                   <div class='daily-each'>
+                       <div class='date-today'>2020-03-02</div>
+                       <div class='daily-note'>今天看到第五章节，对思考关于人的大脑的探索很有趣。</div>
+                       <div class='daily-book'>《思考.快与慢》</div>
+                   </div>
+                   
+
+               </div>
+               
                
            </div>
           
           <div class='booklist'>
+            <div class='read-btn'><div>已读</div></div>
            <h2>在读书单</h2>
+           <ol class='booklist-ol'>
+               <li>美国众神</li>
+               <li>三国演义(听书)</li>
+               <li>思考.快与慢</li>
+               <li>盐糖脂</li>
+               <li>半小时漫画经济学</li>
+           </ol>
           </div>
         </div>
 </template>
@@ -46,87 +70,54 @@ export default {
                 labels: ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
                 datasets: [{
                     label: '数量/本',
-                    data: ['2'],
-                    // backgroundColor: 'yellow'
-                    backgroundColor:['#008080','#ffb6c1','#7fe5f0','#bada55','#ffc3a0','#c0c0c0','#5ac18e','#ffc0cb','#40e0d0','#faebd7'],
+                    data: ['2','4','2','3','5'],
+                    // backgroundColor: 'yellow',
+                    
                     borderWidth: 1,
                     borderColor:'#777',
                     hoverBorderWidth:'2',
-                    hoverBorderColor:'#800080'
+                    hoverBorderColor:'#800080',
+                    
+
+
                 }]
             },    
-            options:{}
+            options:{
+
+            }
         })
+        //-----------------------------------------------------------------------------labels:['小说','经济','思维与认知','心理']
+        let  pieChart=document.querySelector('#pie-chart').getContext('2d')
+        var myPieChart = new Chart(pieChart, {
+          type: 'doughnut',  
+            data: {    //数据存放的地方
+                labels: ['小说','经济','思维与认知','心理'],
+                datasets: [{
+                    label: '数量/本',
+                    data: ['2','4','2','3'],
+                    backgroundColor: ['#FF6384', '#FFCD56','#36A2EB','#40e0d0'],
+                    borderWidth: 1,
+                    hoverBorderColor:'#800080',
+                    
+
+
+                }]
+            },    
+            options:{
+                
+                legend:{
+                    position: 'right'
+                }
+            }
+       });
+
+
     }
 }
 </script>
 
 <style scoped>
- .reading{ 
-     margin-left: 18.6rem;
-     display: flex;
-     
- }
+@import './check.css';
 
- .data-display{
-     flex-basis: 80%;
-     margin-top: 1rem;
-     margin-left: 2rem;
-    
- }
-
-  .summary{
-   display: flex;
-     justify-content: space-between;
-     align-items: center;
-     padding: 0 4rem;
-     height: 13rem;
-     background: white;
- }
-
-.checkout, .finished-task, .notes-num {
-     border: 1px #c1c1c1 solid;
-     border-radius: 50%;
-     font-size: 1.4rem;
-     height: 8rem;
-     width: 8rem;
-     display: flex;
-     justify-content: center;
-     align-items: center;
- }
-
- .checkout:hover, .finished-task:hover, .notes-num:hover{
-     background: #ff7373;
- }
-
- .num{
-     font-size: 1.1rem;
-     text-align: center;
-     padding-top: 0.5rem;
- }
-
- .word{
-     margin-top: 1rem;
- }
-
- .booklist{
-     height: 99vh;
-     margin-left: 2rem;
-     margin-top: 1vh;
-     background: white;
-     flex-basis: 18%;
-     text-align: center;
- }
-
- .readingChart{
-     width:60%;
-    
- }
-
- #myChart{
-   margin-top: 3rem;
-   background: white;
- }
-
-
+ 
 </style>
